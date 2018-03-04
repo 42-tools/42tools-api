e01f65b1d01d16be5da9e9204333dacd6f135d77
Jonathan PHILIPPE <pretrine@hotmail.fr>
2018-03-04 22:34:52 +0100

    Changes visible to hide secret field from apps on admin

--- app/models/forty_two/app.rb
+++ app/models/forty_two/app.rb
@@ -3,6 +3,12 @@ class FortyTwo::App < ApplicationRecord
   has_many :roles,  class_name: 'Role',   dependent: :destroy
   has_many :logs,   class_name: 'Log',    through: :tokens
 
+  rails_admin do
+    configure :secret do
+      hide
+    end
+  end
+
   Role.slugs.keys.each do |slug|
     class_eval <<-METHODS, __FILE__, __LINE__ + 1
       def #{slug}?
