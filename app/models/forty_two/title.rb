666c962c1866e1816f507917675075ce6418d782
Jonathan PHILIPPE <pretrine@hotmail.fr>
2018-03-20 23:52:58 +0100

    Fixes dependent destroy on titles

--- app/models/forty_two/title.rb
+++ app/models/forty_two/title.rb
@@ -1,2 +1,7 @@
 class FortyTwo::Title < ApplicationRecord
+  has_many :achievements, class_name: 'FortyTwo::Achievement'
+
+  before_destroy do |record|
+    record.achievements.update_all(title_id: nil)
+  end
 end
